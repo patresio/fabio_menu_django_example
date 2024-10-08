719e9da32395b3908917789667433c78281ff204
Ricardo Pereira <patresio@gmail.com>
2024-10-08 13:28:25 -0300

    alter slug

--- empresas/models.py
+++ empresas/models.py
@@ -1,20 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
 class Empresa(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save()
 
 
 class Area(models.Model):
     name = models.CharField(max_length=100)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="areas")
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save()
 
 
 class Equipamento(models.Model):
@@ -22,9 +35,15 @@ class Equipamento(models.Model):
     area = models.ForeignKey(
         Area, on_delete=models.CASCADE, related_name="equipamentos"
     )
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save()
 
 
 class Componente(models.Model):
@@ -32,9 +51,15 @@ class Componente(models.Model):
     equipamento = models.ForeignKey(
         Equipamento, on_delete=models.CASCADE, related_name="componentes"
     )
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save()
 
 
 class Proposta(models.Model):
@@ -42,6 +67,12 @@ class Proposta(models.Model):
     componente = models.ForeignKey(
         Componente, on_delete=models.CASCADE, related_name="propostas"
     )
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save()
