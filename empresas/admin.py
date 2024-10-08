aef97f90ff6f6e88b4d65124ed433d24ef69b43e
Ricardo Pereira <patresio@gmail.com>
2024-10-08 11:22:52 -0300

    adjusting new configuration name for id

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from empresas.models import Area, Componente, Empresa, Equipamento, Proposta
+
+
 # Register your models here.
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ("name", "empresa")
+
+
+class EquipamentoAdmin(admin.ModelAdmin):
+    list_display = ("name", "area")
+
+
+class ComponenteAdmin(admin.ModelAdmin):
+    list_display = ("name", "equipamento")
+
+
+class PropostaAdmin(admin.ModelAdmin):
+    list_display = ("name", "componente")
+
+
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Equipamento, EquipamentoAdmin)
+admin.site.register(Componente, ComponenteAdmin)
+admin.site.register(Proposta, PropostaAdmin)
